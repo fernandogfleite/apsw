aea6e1da5ba2c14f59caff7cfd97428913e95163
fernandogfleite <fernando.leite@wlcsolucoes.com.br>
2021-08-23 00:57:59 -0300

    Adição de páginas para listagem, edição e exclusão de usuários

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_bcrypt import Bcrypt
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, EqualTo, Email
-
+from flask_migrate import Migrate
 TEMPLATES = "./templates"
 STATIC = "./static"
 
@@ -21,6 +21,7 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message = 'Faça o login para acessar esta página'
 login_manager.login_message_category = 'info'
+migrate = Migrate(app, db)
 
 
 @login_manager.user_loader
@@ -30,12 +31,13 @@ def load_user(user_id):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
 
 class RegistrationForm(FlaskForm):
-
+    name = StringField('Nome', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Senha', validators=[DataRequired()])
     confirm_password = PasswordField('Confirmar Senha', validators=[
@@ -60,8 +62,6 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user)
             return redirect(url_for('home'))
-        else:
-            flash("Falha no login. Por favor cheque o email e a senha", 'danger')
     return render_template('login.html', form=form)
 
 
@@ -70,20 +70,25 @@ def register():
     if current_user.is_authenticated:
         return redirect(url_for('login'))
     form = RegistrationForm()
-    if form.email.data is not None and form.password.data is not None and form.confirm_password.data is not None:
+    erro = ''
+    if form.email.data is not None and form.password.data is not None and form.confirm_password.data is not None and form.name.data is not None:
         email = User.query.filter_by(email=form.email.data).first()
         if email:
             erro = 'Esse email já está sendo usado, por favor escolha outro'
-            return render_template('register.html', title='Registrar', form=form)
+            return render_template('register.html', title='Registrar', form=form, erro=erro)
+
+        if form.password.data != form.confirm_password.data:
+            erro = 'Senhas não conferem'
+            return render_template('register.html', title='Registrar', form=form, erro=erro)
 
         hashed_password = bcrypt.generate_password_hash(
             form.password.data).decode('utf-8')
-        user = User(email=form.email.data, password=hashed_password)
+        user = User(email=form.email.data, username=form.name.data,
+                    password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash('Sua conta foi criada! Agora você pode fazer login', 'success')
         return redirect(url_for('login'))
-    return render_template('register.html', title='Registrar', form=form)
+    return render_template('register.html', title='Registrar', form=form, erro=erro)
 
 
 @app.route("/logout")
@@ -102,6 +107,48 @@ def home():
     return render_template("index.html", aumento=dados_aumento, reducao=dados_reducao, media=dados_media, data=data)
 
 
-@app.route("/navbar")
-def navbar():
-    return render_template("navbar.html")
+@app.route("/users")
+def users():
+    usuarios = User.query.all()
+    return render_template("index-admin.html", usuarios=usuarios)
+
+
+@app.route("/users/<int:user_id>", methods=["GET", "POST"])
+def edit(user_id):
+    user = User.query.get_or_404(user_id)
+    form = RegistrationForm()
+    erro = ''
+    if form.email.data is not None and form.password.data is not None and form.confirm_password.data is not None and form.name.data is not None:
+        email = User.query.filter_by(email=form.email.data).first()
+        if email:
+            if email != user:
+                flash(
+                    'Esse email já está sendo usado, por favor escolha outro', 'danger')
+                return render_template('edit-admin.html', form=form, erro=erro)
+        if form.password.data != form.confirm_password.data:
+            flash('Senhas não coincidem', 'danger')
+            return render_template('edit-admin.html', form=form, erro=erro)
+
+        user.email = form.email.data
+        user.username = form.name.data
+        user.password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        db.session.commit()
+        form.name.data = user.username
+        form.email.data = user.email
+        flash('Mudanças salvas com sucesso!', 'success')
+
+        return render_template('edit-admin.html', form=form, erro=erro)
+    if request.method == "GET":
+        form.name.data = user.username
+        form.email.data = user.email
+
+    return render_template('edit-admin.html', form=form, erro=erro)
+
+
+@app.route("/delete_user/<int:user_id>", methods=['POST'])
+def delete_user(user_id):
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for('users'))
